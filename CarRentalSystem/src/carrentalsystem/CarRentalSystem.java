e33e83201582bf81ed26511bf4bc6c397f28f5dc
zahin123 <114458369+zahin123@users.noreply.github.com>
2024-07-21 00:43:40 -0500

    Add files via upload

---
+++ CarRentalSystem/src/carrentalsystem/CarRentalSystem.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package carrentalsystem;
+
+/**
+ *
+ * @author 2010a
+ */
+public class CarRentalSystem {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
